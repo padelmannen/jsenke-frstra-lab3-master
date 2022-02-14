9afd10188006e03cc8e0536ec8a281bdaa9fbc1e
padelmannen <jesper.senke@gmail.com>
2022-02-14 19:43:14 +0100

    commit

--- src/base/routers/profile.router.js
+++ src/base/routers/profile.router.js
@@ -10,7 +10,7 @@ privateRouter.get("/", async (req, res) => {
   // console.log(req.body);
   // console.log("req", req)
   console.log("kör privat router profile")
-  console.log(req.cookie)
+  console.log(req.headers.cookie)
   console.log(req.method)
 
   const htmlDoc = (await readFile(resolvePublicPath("index.html"))).replace(
