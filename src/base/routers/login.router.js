8544d994c3050d7a731d61f8b215576cb3ab08f4
padelmannen <jesper.senke@gmail.com>
2022-02-16 20:23:58 +0100

    fel

--- src/base/routers/login.router.js
+++ src/base/routers/login.router.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { readFile, resolvePublicPath } from "../util.js";
-import sessionManager from "../sessionManager.js";
+// import sessionManager from "../sessionManager.js";
 
 const publicRouter = Router();
 
@@ -13,27 +13,48 @@ publicRouter.get("/login", async (req, res) => {
   console.log("kör pub router login");
   // console.log(res)
 
-  if(!sessionManager.findSessionById(req.headers.cookie.split("=")[1])){
-    const htmlDoc = await readFile(resolvePublicPath("login.html"));
-    res.status(200).send(htmlDoc);
-  }
-  else{
+  try{
+    req.headers.cookie.split("=")
     console.log("ERROR: kan inte gå till login om inloggad");
     res.redirect("/");
   }
+  catch(TypeError){
+    const htmlDoc = await readFile(resolvePublicPath("login.html"));
+    res.status(200).send(htmlDoc);
+  }
+ 
+ 
+  // if(!sessionManager.findSessionById(req.headers.cookie.split("=")[1])){
+  //     const htmlDoc = await readFile(resolvePublicPath("login.html"));
+  //     res.status(200).send(htmlDoc);
+  //   }
+  // else{
+  //   console.log("ERROR: kan inte gå till login om inloggad");
+  //   res.redirect("/");
+  // }
 
 });
 
 publicRouter.get("/registration", async (req, res) => {
   console.log("kör pub router reqistration");
   
-  if(!sessionManager.findSessionById(req.headers.cookie.split("=")[1])){
-    const htmlDoc = await readFile(resolvePublicPath("registration.html"));
-    res.status(200).send(htmlDoc);
-  }
-  else{
+  // if(!sessionManager.findSessionById(req.headers.cookie.split("=")[1])){
+  //   const htmlDoc = await readFile(resolvePublicPath("registration.html"));
+  //   res.status(200).send(htmlDoc);
+  // }
+  // else{
+  //   console.log("ERROR: kan inte gå till registration om inloggad");
+  //   res.redirect("/");
+  // }
+
+  try{
+    req.headers.cookie.split("=")
     console.log("ERROR: kan inte gå till registration om inloggad");
     res.redirect("/");
+  }
+  catch(TypeError){
+    const htmlDoc = await readFile(resolvePublicPath("registration.html"));
+    res.status(200).send(htmlDoc);
   }
 
 });
